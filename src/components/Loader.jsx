b698f3375c974dff7210997b3157d5a10b2ee113
Eber Leguizamon <eberlegza@gmail.com>
2022-09-21 17:06:37 -0300

    add prop-types to loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import { ThreeDots } from 'react-loader-spinner'
+import { PropTypes } from 'prop-types'
 
-function Loader() {
-  return (
-    <ThreeDots
-      height="70"
-      width="70"
-      radius="6"
-      color="#000"
-      ariaLabel="three-dots-loading"
-      visible
-    />
-  )
+const Loader = ({
+  height, width, color, radius,
+}) => (
+  <ThreeDots
+    height={height}
+    width={width}
+    radius={radius}
+    color={color}
+    ariaLabel="three-dots-loading"
+    visible
+  />
+)
+
+Loader.propTypes = {
+  height: PropTypes.string,
+  width: PropTypes.string,
+  color: PropTypes.string,
+  radius: PropTypes.string,
 }
 
+Loader.defaultProps = {
+  height: '70',
+  width: '70',
+  color: '#000',
+  radius: '6',
+}
 export default Loader
