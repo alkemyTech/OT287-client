8c3a8ab7263f9179cdac3b915560aeb8c58db6a1
Federico Capucci <federicocapucci@hotmail.com>
2022-09-22 11:02:33 -0300

    [OT287-19] Setup React Router (#3)
    * ReactRouter installed, routes added
    
    * Edited test file
    
    * Edited test file
    
    * Moved dummy components to pages folder, moved router to routes folder

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import store from './app/store';
+import { BrowserRouter as Router} from "react-router-dom";
 import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+    <App />
+  </Router>,
   );
 
   expect(getByText(/learn/i)).toBeInTheDocument();
