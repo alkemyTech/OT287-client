eaf946da4842d817ea2a1ca5389483d7bfd35190
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-09-21 22:22:44 -0300

    Added burger menu component

---
+++ src/components/Header/Navbar/BurgerMenu.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+function BurgerMenu({
+  menu, handleOpenMenu, handleCloseMenu, anchorNav,
+}) {
+  return (
+    <Box sx={{ display: { xs: 'flex', md: 'none' }, justifyContent: 'right', mr: 4 }}>
+      <IconButton
+        size="large"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleOpenMenu}
+        color="inherit"
+      >
+        <MenuIcon sx={{ color: 'black' }} />
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorNav}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorNav)}
+        onClose={handleCloseMenu}
+        sx={{
+          display: { xs: 'block', md: 'none' },
+        }}
+      >
+        {menu.map((page) => (
+          <MenuItem key={page.id} onClick={handleCloseMenu}>
+            <Typography textAlign="center">{page.text}</Typography>
+          </MenuItem>
+        ))}
+      </Menu>
+    </Box>
+  )
+}
+
+BurgerMenu.propTypes = {
+  menu: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    text: PropTypes.string,
+    ruote: PropTypes.string,
+  })).isRequired,
+  handleOpenMenu: PropTypes.func.isRequired,
+  handleCloseMenu: PropTypes.func.isRequired,
+  anchorNav: PropTypes.instanceOf(Element),
+}
+
+BurgerMenu.defaultProps = {
+  anchorNav: null,
+}
+
+export default BurgerMenu
