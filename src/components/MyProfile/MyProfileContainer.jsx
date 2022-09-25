6fedb8e7e8c879614ac933c29e27242e5daf1cd4
eli-beg <begaeliana@gmail.com>
2022-09-25 11:58:48 -0300

    Fix comment in MyProfileContainer

--- src/components/MyProfile/MyProfileContainer.jsx
+++ src/components/MyProfile/MyProfileContainer.jsx
@@ -7,7 +7,7 @@ import MyProfile from './MyProfile'
 
 // la ruta '/me' es un get que devuelve la información del usuario contenida en un token.
 // A modo de ejemplo se puse utilizar un token válido y setearlo en el header de axios
-axios.defaults.headers.common.Authorization = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZmlyc3ROYW1lIjoiZWxpIiwibGFzdE5hbWUiOiJiIiwiZW1haWwiOiJlbGlAZ21haWwuY29tIiwiaW1hZ2UiOm51bGwsInBhc3N3b3JkIjoiRWxpODg4ODgiLCJyb2xlSWQiOjIsImRlbGV0ZWRBdCI6bnVsbCwiY3JlYXRlZEF0IjoiMjAxMi0xMi0yNVQwMDowMDowMC4wMDBaIiwidXBkYXRlZEF0IjoiMjAxMi0xMi0yNVQwMDowMDowMC4wMDBaIiwiaWF0IjoxNjYzODUyNDEwfQ.OZG6p_bJGgA90Kmxd89T0cEnceVDWBCBdsCXNhTwbGM'
+// axios.defaults.headers.common.Authorization ='Bearer tokenValido'
 
 const MyProfileContainer = () => {
   const [user, setUser] = useState(null)
