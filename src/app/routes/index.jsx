961300cc6928f67862dbf31daded8a6f4691e1c0
Corxario <federicocapucci@hotmail.com>
2022-09-25 18:50:29 -0300

    Deleting unnecessary files

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/dummyHome/home'
 import About from '../pages/dummyAbout/about'
-import RegisterFormContainer from '../../components/RegisterForm/RegisterFormContainer'
 import LoginFormContainer from '../../components/LoginForm/LoginFormContainer'
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="about" element={<About />} />
-    <Route path="/register" element={<RegisterFormContainer />} />
     <Route path="/login" element={<LoginFormContainer />} />
   </Routes>
 )
