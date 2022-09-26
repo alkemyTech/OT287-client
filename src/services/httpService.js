d03eb325e3aea8f98945b417352ddb5edab1ee0e
MrLuis-WebMaster <mr.luiswebmaster@gmail.com>
2022-09-26 18:31:08 -0500

    validation token for httpService

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -3,15 +3,23 @@ import axios from 'axios'
 const httpService = async (method, url, body) => {
   try {
     const token = JSON.parse(localStorage.getItem('token'));
+    let headers = {}
+    if (token) {
+      headers = {
+        'Content-type': 'application/json; charset=UTF-8',
+        Authorization: `Bearer ${token}`,
+      }
+    } else {
+      headers = {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    }
     const { data } = await axios({
       method: `${method}`,
       baseURL: 'http://localhost:3001',
       url: `${url}`,
       data: body,
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-        Authorization: `Bearer ${token}`,
-      },
+      headers: {...headers},
     })
     return data
   } catch (error) {
