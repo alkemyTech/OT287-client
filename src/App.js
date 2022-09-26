f175f28e0abdbbfd7453d77e4ad5a74edb652b0c
Gabriel <pein4302@gmail.com>
2022-09-25 23:25:32 -0300

    added styles beta

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 
 const App = () => (
   <>
+    <h1>Welcome to learn React Router!</h1>
     <Router />
   </>
 )
--- src/app/pages/home/Home.jsx
+++ src/app/pages/home/Home.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {
-  Toolbar, Box, Stack, Typography,
+  Toolbar, Box, Stack, Typography, Grid, Button
 } from '@mui/material'
 import Slider from '../../../components/Slider/Slider'
 
-const Home = ({ news, slider }) => {
-  console.log(news)
-  return (
-    <div>
-      <h1>Bienvenido</h1>
-      <Slider items={slider} />
-      {news.length > 1 ? news.map((elem) => (
-        <Box
-          key={elem.createdAt}
-        >
-          <h1>{elem.name}</h1>
-          <p>{elem.content}</p>
-          <img src={elem.image} alt="" />
+const Home = ({ news, slider }) => (
+  <Box>
+    <h1 style={{ textAlign: 'left' }}>Bienvenido</h1>
+    <Slider items={slider} />
+    {news.length > 1 ? news.map((elem) => (
 
-        </Box>
-      )) : <p>no news to show</p>}
-    </div>
-  )
-}
+      <Grid sx={{ gridTemplateColumns: '10px 10px' }} key={elem.createdAt} container>
+        <Grid item xs={7} lg={6}>
+          <Box sx={{ backgroundColor: '#4c9ff8' }} border={3} borderColor="#9ac9fb" borderRadius={1}>
+            <Typography sx={{ textAlign: 'center', position: { lg: 'relative' },  left: { xs: '100px', lg: ' 400px' }, width: { xs: 'auto', lg: '400px' } }} component="h1">
+              {elem.name}
+              <p style={{ wordBreak: 'break-all' }}>{elem.content}</p>
+              <Button sx={{ color: 'white' }}>ver novedad</Button>
+            </Typography>
+            <img style={{ width: '40px', height: '40px', float: 'left' }} src={elem.image} alt="" />
+          </Box>
+        </Grid>
+        <Grid>we</Grid>
+      </Grid>
+
+    )) : <p>no news to show</p>}
+  </Box>
+)
 
 Home.propTypes = {
   news: PropTypes.arrayOf(PropTypes.shape({
