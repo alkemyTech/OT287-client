b60e4172ab92a13d0ee30f576d741148567094da
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-09-26 11:30:37 -0300

    added back-office route

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/dummyHome/home'
 import About from '../pages/dummyAbout/about'
+import BackOfficeContainer from "../../components/BackOffice/BackOfficeContainer"
 
 const Router = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="about" element={<About />} />
+    <Route path="/backoffice" element={<BackOfficeContainer />} />
   </Routes>
 )
 
