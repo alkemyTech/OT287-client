834571bd3467e02c30407791f949a73165450799
eli-beg <begaeliana@gmail.com>
2022-09-27 08:52:18 -0300

    Add conection of Navbar with routes

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import { Box, Button } from '@mui/material';
 
-const Navbar = ({ menu }) => {
-  return (
-    <Box sx={{ display: { xs: 'none', md: 'flex' }, flexGrow: 1, justifyContent: 'right' }}>
-      {menu.map((page) => (
-        <Button
-          key={page.id}
-          sx={{
-            my: 2, mx: { md: 0.5, lg: 0.8 }, display: 'block', color: 'black', fontSize: { lg: '1rem' },
-          }}
-        >
-          {page.text}
-        </Button>
-      ))}
-    </Box>
-  )
-}
+const Navbar = ({
+  menu, navigate, handleActiveButton, activeButton,
+}) => (
+  <Box sx={{ display: { xs: 'none', md: 'flex' }, flexGrow: 1, justifyContent: 'right' }}>
+    {menu.map((page) => (
+
+      <Button
+        key={page.id}
+        onClick={() => {
+          handleActiveButton(page.route)
+          navigate(page.route)
+        }}
+        sx={{
+          my: 2, mx: { md: 0.5, lg: 0.8 }, display: 'block', color: 'black', fontSize: { lg: '1rem' }, backgroundColor: activeButton === page.route ? '#ffcdd2' : null,
+        }}
+      >
+        {page.text}
+      </Button>
+
+    ))}
+  </Box>
+)
 
 Navbar.propTypes = {
   menu: PropTypes.arrayOf(PropTypes.shape({
@@ -26,6 +31,9 @@ Navbar.propTypes = {
     text: PropTypes.string,
     route: PropTypes.string,
   })).isRequired,
+  navigate: PropTypes.func.isRequired,
+  handleActiveButton: PropTypes.func.isRequired,
+  activeButton: PropTypes.string.isRequired,
 }
 
 export default Navbar
