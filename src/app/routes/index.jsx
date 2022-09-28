b17d4cd72102708a3f90f39205a7af670a95e974
GregoHouse <96206226+GregoHouse@users.noreply.github.com>
2022-09-28 09:53:30 -0300

    [OT287 -48] : Screen News List  (#22)
    * OT287-48 screen news list
    
    * OT287-48 refactor import mui components
    
    * OT287-48 http service
    
    Co-authored-by: “GregoryHouse” <“gregorioromerogarzon@gmail.com”>

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -6,8 +6,10 @@ import BackOfficeContainer from '../../components/BackOffice/BackOfficeContainer
 import HomeFormContainer from '../../components/Form/HomeFormContainer'
 import LoginFormContainer from '../../components/LoginForm/LoginFormContainer'
 import RegisterFormContainer from '../../components/RegisterForm/RegisterFormContainer'
+import NewsContainer from '../../components/News/NewsContainer'
 import NewsByIdContainer from '../../components/News/NewByIdContainer'
 import MyProfileContainer from '../../components/MyProfile/MyProfileContainer'
+
 
 const Router = () => (
   <Routes>
@@ -17,8 +19,10 @@ const Router = () => (
     <Route path="/homeForm" element={<HomeFormContainer />} />
     <Route path="/register" element={<RegisterFormContainer />} />
     <Route path="/login" element={<LoginFormContainer />} />
+    <Route path="/news" element={<NewsContainer />} />
     <Route path="/news/:id" element={<NewsByIdContainer />} />
     <Route path="/myprofile" element={<MyProfileContainer />} />
+
   </Routes>
 )
 
