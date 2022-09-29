24c235a6c0730d19f1d50d25d8d45943ffa77b91
Gabriel <pein4302@gmail.com>
2022-09-29 15:19:42 -0300

    fix prettierSlint error

--- src/components/Home/HomeContainer.jsx
+++ src/components/Home/HomeContainer.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Home from './Home'
 import httpService from '../../services/httpService'
-import FooterContainer from '../footer/FooterContainer'
 
 const sliderImg = [
   {
