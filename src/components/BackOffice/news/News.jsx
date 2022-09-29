f61728d83257d27d4f7b97c4fdb3727e64a46eec
Corxario <federicocapucci@hotmail.com>
2022-09-29 16:24:18 -0300

    Improve mobile View and looks

--- src/components/BackOffice/news/News.jsx
+++ src/components/BackOffice/news/News.jsx
@@ -11,7 +11,7 @@ const Users = ({ news }) => (
     <Typography component="h1" variant="h5" sx={{ marginY: { lg: '40px', xs: '10px' }, fontWeight: 'bold' }}>Lista de Novedades</Typography>
     <TableContainer
       sx={{
-        position: 'absolute', top: '180px', height: '400px', overflow: 'auto', width: { lg: '85%', xs: '100vw' },
+        position: 'absolute', top: '180px', height: '400px', overflow: 'auto', width: { lg: '80%', xs: '100%' },
       }}
       component={Paper}
     >
@@ -29,9 +29,9 @@ const Users = ({ news }) => (
 
             <TableRow key={elem.id}>
               <TableCell>{elem.name}</TableCell>
-              <TableCell>{elem.image}</TableCell>
-              <TableCell>{elem.createdAt}</TableCell>
-              <TableCell style={{ padding: '0', width: '60px' }} align="center">
+              <TableCell sx={{ width: '30px', overflow: 'hidden', textOverflow: 'ellipsis' }}>{elem.image}</TableCell>
+              <TableCell sx={{ width: '220px', overflow: 'hidden', textOverflow: 'ellipsis' }}>{elem.createdAt}</TableCell>
+              <TableCell sx={{ padding: '0', width: '60px' }} align="center">
                 <>
                   <EditIcon sx={{
                     opacity: '0.5', padding: '1px', border: '1px solid red', borderRadius: '5px', backgroundColor: 'white', color: 'red', fontSize: '1.8rem', margin: '0 5px', cursor: 'pointer', '&:hover': { opacity: '1' },
