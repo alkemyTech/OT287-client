81f63ee4881a0fd0a312927434b324d242dc9f5b
Corxario <federicocapucci@hotmail.com>
2022-09-29 21:38:54 -0300

    Deleting unnecessary console log on login

--- src/components/Forms/LoginForm/LoginFormContainer.jsx
+++ src/components/Forms/LoginForm/LoginFormContainer.jsx
@@ -15,8 +15,6 @@ const LoginFormContainer = () => {
         email: values.email,
         password: values.password,
       })
-      // eslint-disable-next-line no-console
-      console.log(data)
       if (data.code === 200) {
         navigate('/')
       } else {
