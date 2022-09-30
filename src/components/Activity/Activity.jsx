456392293dd297fe42d783a215ccf3350ed66c5a
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-09-30 18:35:54 -0300

    Added activity desktop view

--- src/components/Activity/Activity.jsx
+++ src/components/Activity/Activity.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import { Typography, Container, Box } from '@mui/material'
+import {
+  Typography, Container, Box, Grid,
+} from '@mui/material'
 
 // eslint-disable-next-line no-unused-vars
 const Activity = ({ data, loading, error }) => {
@@ -13,10 +15,23 @@ const Activity = ({ data, loading, error }) => {
   }
 
   return (
-    <Container sx={{ my: 10 }}>
-      <Typography variant="h2" sx={{ fontSize: '1.5rem', fontWeight: 600 }}>{ data.name }</Typography>
-      <Typography variant="body2" component="p" sx={{ mt: 1, mb: 2 }}>{ data.content }</Typography>
-      <Box component="img" src={data.image} alt={data.name} sx={{ width: '100%', borderRadius: '1.25rem' }} />
+    <Container sx={{ my: { xs: 10, md: 14 } }}>
+      <Grid container justifyContent="center">
+        <Grid item md={5} sx={{ mr: { md: 5 } }}>
+          <Typography variant="h2" sx={{ fontSize: '1.5rem', fontWeight: 600, maxWidth: '37.5rem' }}>{ data.name }</Typography>
+          <Typography variant="body2" component="p" sx={{ mt: 1, mb: 2, maxWidth: '37.5rem' }}>{ data.content }</Typography>
+        </Grid>
+        <Grid item md={4}>
+          <Box
+            component="img"
+            src={data.image}
+            alt={data.name}
+            sx={{
+              width: '100%', borderRadius: '1.25rem', maxWidth: { xs: '22rem', md: '18.75rem' }, transform: { md: 'rotate(5deg)' },
+            }}
+          />
+        </Grid>
+      </Grid>
     </Container>
   )
 }
