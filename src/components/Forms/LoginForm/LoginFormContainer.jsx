c0fbdad76e350cd83f7e5d866cb2a562e3dfdd49
Corxario <federicocapucci@hotmail.com>
2022-09-30 01:09:46 -0300

    Add logout (erase user data) functionality, Dynamic header buttons

--- src/components/Forms/LoginForm/LoginFormContainer.jsx
+++ src/components/Forms/LoginForm/LoginFormContainer.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { setUserData } from '../../../app/slices/auth/authSlice'
 import LoginForm from './LoginForm'
 import validateLogin from '../../../schemas/login';
 import httpService from '../../../services/httpService';
@@ -7,6 +9,7 @@ import httpService from '../../../services/httpService';
 const LoginFormContainer = () => {
   const [errorStatus, setErrorStatus] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const dispatch = useDispatch()
   const navigate = useNavigate()
 
   const onSubmitForm = async (values) => {
@@ -16,7 +19,15 @@ const LoginFormContainer = () => {
         password: values.password,
       })
       if (data.code === 200) {
+        const userData = {
+          id: data.body.user.id,
+          userName: `${data.body.user.firstName} ${data.body.user.lastName}`,
+          image: data.body.user.image,
+          token: data.body.token,
+        }
+        dispatch(setUserData(userData))
         navigate('/')
+        window.location.reload()
       } else {
         setErrorStatus(data.response.status)
         setErrorMessage(data.response.statusText)
--- src/components/Forms/RegisterForm/RegisterFormContainer.jsx
+++ src/components/Forms/RegisterForm/RegisterFormContainer.jsx
@@ -34,10 +34,11 @@ const RegisterFormContainer = () => {
             token: regData.body.token,
           }
           dispatch(setUserData(userData))
+          navigate('/')
+          window.location.reload()
         } catch (error) {
           setErrorStatus(`Ha ocurrido un error: ${error.response}`)
         }
-        navigate('/')
       } else {
         setErrorStatus(data.response.status)
         setErrorMessage(data.response.statusText)
