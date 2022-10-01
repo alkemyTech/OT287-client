217d49c50ee4208f5a93d9a795fe254628dd77e4
MrLuis-WebMaster <mr.luiswebmaster@gmail.com>
2022-10-01 10:59:45 -0500

    add component by privated route

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-export const PrivateRoute = ({component}) => {
-  return (
-    <div>PrivateRoute</div>
-  )
+export const PrivateRoute = ({children}) => {
+  const user = useSelector(state => state.auth.userData)
+  const navigate = useNavigate()
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
+    }
+  }, [user, navigate]);
+  if (user && user.roleId === 1) return children;
+  return children;
 }
