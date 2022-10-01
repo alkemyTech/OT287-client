9750a8c99b574797376e1485f0f079c3bd1455f0
Guiniote <guiniote@gmail.com>
2022-10-01 19:05:32 -0300

    AWS File component modifications

--- src/components/Layout/AWSFileUpload.js
+++ src/components/Layout/AWSFileUpload.js
@@ -1,6 +1,8 @@
-// import S3 from 'react-aws-s3'
+// IMPORTANTE: FALTA SETEAR EL BUCKET COMO PUBLIC-READ PARA QUE PUEDA FUNCIONAR.
 
-const AWSFileUpload = async (file) => /* {
+import S3 from 'react-aws-s3'
+
+const AWSFileUpload = async (file) => {
   const config = {
     bucketName: process.env.REACT_APP_AWS_BUCKET_NAME,
     dirName: process.env.REACT_APP_AWS_BUCKET_DIRNAME,
@@ -13,16 +15,13 @@ const AWSFileUpload = async (file) => /* {
 
   await ReactS3Client
     .uploadFile(file)
-    .then((data) => {
-      // eslint-disable-next-line no-console
-      console.log(data)
-      return data
-    })
+    .then((data) => data)
     .catch((err) => {
       // eslint-disable-next-line no-console
       console.error(err)
     })
-  } */
-  `https://myBucket.s3.amazonaws.com/${file.name}`
+}
+
+// EJEMPLO DE CÓMO SERÍA LA RUTA QUE DEVUELVE: `https://myBucket.s3.amazonaws.com/${file.name}`
 
 export default AWSFileUpload
