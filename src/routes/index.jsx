8cb2f06c9e95e68e1bff4220de008ac0d9915b1f
Eber Leguizamon <eberlegza@gmail.com>
2022-10-02 20:14:55 -0300

    feat: add edit form for users and admins

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -13,6 +13,8 @@ import EditOrganizationContainer from '../components/Forms/OrganizationForm/Edit
 import BackofficeUsers from '../components/BackOffice/users/UsersContainer'
 import BackofficeNews from '../components/BackOffice/news/NewsContainer'
 import Logout from '../components/Logout/Logout'
+import EditUserProfileContainer from '../components/Forms/EditUserProfile/EditUserProfileContainer'
+import EditUserContainer from '../components/BackOffice/UserEditFormBackOffice/EditUserContainer'
 
 const Router = () => (
   <Routes>
@@ -22,6 +24,7 @@ const Router = () => (
       <Route path="/login" element={<LoginFormContainer />} />
       <Route path="/registrate" element={<RegisterFormContainer />} />
       <Route path="/mi-perfil" element={<MyProfileContainer />} />
+      <Route path="/mi-perfil/editar" element={<EditUserProfileContainer />} />
       <Route path="/novedades" element={<NewsContainer />} />
       <Route path="/novedades/:id" element={<NewsByIdContainer />} />
       <Route path="/contacto" element={<ContactScreen />} />
@@ -29,6 +32,7 @@ const Router = () => (
       <Route path="/back-office" element={<BackOfficeContainer />}>
         <Route path="organization-edit" element={<EditOrganizationContainer />} />
         <Route path="users" element={<BackofficeUsers />} />
+        <Route path="users/:id" element={<EditUserContainer />} />
         <Route path="news" element={<BackofficeNews />} />
         <Route path="news/:id" element={<BackofficeNews />} />
 
