19fc74aeae76a4f1a7d96a29bfccfd363030533b
Eber Leguizamon <eberlegza@gmail.com>
2022-10-02 21:45:03 -0300

    fix: add navigate link  from users table to user edit form

--- src/components/BackOffice/users/Users.jsx
+++ src/components/BackOffice/users/Users.jsx
@@ -5,39 +5,43 @@ import {
 } from '@mui/material'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import EditIcon from '@mui/icons-material/Edit';
+import { useNavigate } from 'react-router-dom';
 
-const Users = ({ users }) => (
-  <Box>
-    <TableContainer sx={{ position: 'absolute', top: '80px', width: { lg: '85%', xs: '100%' } }} component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell>Nombre</TableCell>
-            <TableCell>Apellido</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Editar</TableCell>
-            <TableCell>Eliminar</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map((user) => (
-
-            <TableRow key={user.id}>
-              <TableCell>{user.id}</TableCell>
-              <TableCell>{user.firstName}</TableCell>
-              <TableCell>{user.lastName}</TableCell>
-              <TableCell>{user.email}</TableCell>
-              <TableCell sx={{ color: 'yellow' }}><EditIcon /></TableCell>
-              <TableCell sx={{ color: 'red' }}><HighlightOffIcon /></TableCell>
+const Users = ({ users }) => {
+  const navigate = useNavigate()
+  return (
+    <Box>
+      <TableContainer sx={{ position: 'absolute', top: '80px', width: { lg: '85%', xs: '100%' } }} component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Id</TableCell>
+              <TableCell>Nombre</TableCell>
+              <TableCell>Apellido</TableCell>
+              <TableCell>Email</TableCell>
+              <TableCell>Editar</TableCell>
+              <TableCell>Eliminar</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  </Box>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
 
-)
+              <TableRow key={user.id}>
+                <TableCell>{user.id}</TableCell>
+                <TableCell>{user.firstName}</TableCell>
+                <TableCell>{user.lastName}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell sx={{ color: 'yellow' }}><EditIcon onClick={() => { navigate(`/back-office/users/${user.id}`) }} /></TableCell>
+                <TableCell sx={{ color: 'red' }}><HighlightOffIcon /></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+
+  )
+}
 Users.propTypes = {
   users: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number,
