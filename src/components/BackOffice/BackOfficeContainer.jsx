6451eba9ec01d9189834032839e90443e6121a9b
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-10-03 19:20:48 -0300

    Removed fake data

--- src/components/BackOffice/BackOfficeContainer.jsx
+++ src/components/BackOffice/BackOfficeContainer.jsx
@@ -22,12 +22,12 @@ const array = {
       image: 'https://via.placeholder.com/500x500.png',
     },
   ],
-  categories: [
+  activities: [
     {
       id: 1,
       name: 'Lorem Ipsum',
-      description: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-      image: null,
+      content: "y. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      image: 'https://via.placeholder.com/500x500.png',
     },
   ],
   testimonials: [
