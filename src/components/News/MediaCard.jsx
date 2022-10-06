065f54aed828a16225f32dcd935811ee854aff05
eli-beg <begaeliana@gmail.com>
2022-10-06 10:20:02 -0300

    Fix grammar error

--- src/components/News/MediaCard.jsx
+++ src/components/News/MediaCard.jsx
@@ -32,14 +32,7 @@ const MediaCard = ({ data }) => (
           {data.name}
         </Typography>
         <Typography className="contentNewAbstract" color="#000000" sx={{ fontSize: '12px' }}>
-          {/* {data.content} */}
-          {' '}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-          deserunt mollit anim id est laborum.
+          {data.content}
         </Typography>
       </CardContent>
       <CardActions sx={{
