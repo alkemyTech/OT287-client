a2ae0038e34e186527c9891575970ff91f44590d
Corxario <federicocapucci@hotmail.com>
2022-10-11 22:44:02 -0300

    Adding functionality to profile/backoffice button depending on role

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react'
 // import { useSelector } from 'react-redux'
 import { useNavigate, useLocation } from 'react-router-dom';
-import Header from './Header'
 import LoginIcon from '@mui/icons-material/Login';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import Header from './Header'
 
 const HeaderContainer = () => {
   const [anchorNav, setAnchorNav] = React.useState(null);
   const [activeButton, setActiveButton] = useState('/')
   // const userData = useSelector((state) => state.auth.userData);
   const isLogged = Boolean(localStorage.getItem('user'))
+  let isAdmin = false
+  if (isLogged) {
+    isAdmin = Boolean(JSON.parse(localStorage.getItem('user')).roleId === 1)
+  }
+
   const data = {
     logo: 'https://i.imgur.com/nIclrvm.png',
     menu: [
@@ -48,27 +53,33 @@ const HeaderContainer = () => {
       {
         id: 7,
         route: (isLogged ? '/logout' : '/login'),
-        icon: (isLogged ? <ExitToAppIcon
-          sx={{
-            color:'#DB5752', 
-            width:'auto', 
-            height:{sx:'35px', sm:'30px', md:'40px'}, 
-            margin:'10px',
+        icon: (isLogged ? (
+          <ExitToAppIcon
+            sx={{
+              color: '#DB5752',
+              width: 'auto',
+              height: { sx: '35px', sm: '30px', md: '40px' },
+              margin: '10px',
             }}
-        /> : <LoginIcon
-        sx={{
-          color:'#DB5752', 
-          width:'auto', 
-          height:{sx:'35px', sm:'30px', md:'40px'}, 
-          margin:'10px' 
-          }}
-        /> ),
-        text:(isLogged ? 'Log out' : 'Log in')
+          />
+        ) : (
+          <LoginIcon
+            sx={{
+              color: '#DB5752',
+              width: 'auto',
+              height: { sx: '35px', sm: '30px', md: '40px' },
+              margin: '10px',
+            }}
+          />
+        )),
+        text: (isLogged ? 'Log out' : 'Log in'),
       },
       {
         id: 8,
-        route: (isLogged ? '/mi-perfil' : '/registrate'),
-        text: (isLogged ? 'Mi Perfil' : 'Registrate'),
+        // eslint-disable-next-line no-nested-ternary
+        route: (isLogged ? isAdmin ? '/back-office' : '/mi-perfil' : '/registrate'),
+        // eslint-disable-next-line no-nested-ternary
+        text: (isLogged ? isAdmin ? 'Back Office' : 'Mi Perfil' : 'Registrate'),
       },
     ],
   }
