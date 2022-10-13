6808ba2a2cf053447003e4b0e03663bf19e293e6
Federico Capucci <federicocapucci@hotmail.com>
2022-10-13 09:06:58 -0300

    [OT287-Fix-#013] News Table layout Overhaul to add additional content (#96)
    * Layout and buttons changes in contact section
    
    * News Table, Table overhaul for adding data
    
    * Adding sticky header and primary colors to buttons

--- src/components/BackOffice/news/News.jsx
+++ src/components/BackOffice/news/News.jsx
@@ -9,6 +9,10 @@ import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 import EditIcon from '@mui/icons-material/Edit'
 import DeleteModal from '../../Layout/DeleteModal'
 
+function isImage(url) {
+  return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
+}
+
 const News = (
   props,
 ) => {
@@ -37,11 +41,12 @@ const News = (
           }}
           component={Paper}
         >
-          <Table>
+          <Table stickyHeader>
             <TableHead>
-              <TableRow sx={{ backgroundColor: 'rgb(240,240,240)' }}>
+              <TableRow>
                 <TableCell><b>Nombre</b></TableCell>
-                <TableCell><b>URL Imagen</b></TableCell>
+                <TableCell align="center"><b>Imagen</b></TableCell>
+                <TableCell><b>Contenido</b></TableCell>
                 <TableCell><b>Fecha creacion</b></TableCell>
                 <TableCell align="center"><b>Acciones</b></TableCell>
               </TableRow>
@@ -50,43 +55,54 @@ const News = (
 
               {news && news.map((elem) => (
 
-                <TableRow key={elem.id}>
+                <TableRow key={elem.id} height="60px">
                   <TableCell
-                    sx={{ width: '25%', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                    sx={{ width: '15%', overflow: 'hidden', textOverflow: 'ellipsis' }}
                   >
                     {elem.name}
                   </TableCell>
                   <TableCell
-                    sx={{ width: '25%', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                    align="center"
+                    sx={{
+                      margin: '1px', padding: '0', width: '10%',
+                    }}
                   >
                     <Box
                       component="img"
                       sx={{
-                        height: 233,
-                        width: 250,
-                        maxHeight: { xs: 120, md: 167 },
-                        maxWidth: { xs: 140, md: 250 },
+                        margin: '3px auto',
+                        height: '60px',
+                        width: '60px',
+                        objectFit: 'cover',
+                        borderRadius: '50%',
                       }}
-                      src={elem.image}
+                      src={isImage(elem.image) || elem.image.length > 10 ? elem.image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKH2cSl_mcI1heNKNhcs7Qpg0OVCh8AsiD5A&usqp=CAU'}
                       alt="news image"
                     />
                   </TableCell>
                   <TableCell
-                    sx={{ width: '25%', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                    sx={{
+                      width: '45%', textOverflow: 'ellipsis',
+                    }}
+                  >
+                    {elem.content.length < 80 ? elem.content : `${elem.content.substring(0, 80)}...` }
+                  </TableCell>
+                  <TableCell
+                    sx={{ width: '15%', overflow: 'hidden', textOverflow: 'ellipsis' }}
                   >
                     {elem.createdAt}
 
                   </TableCell>
-                  <TableCell sx={{ padding: '0', width: '25%' }} align="center">
+                  <TableCell sx={{ padding: '0', width: '10 %' }} align="center">
                     <>
                       <Link to={`${elem.id}/edit`}>
                         <EditIcon sx={{
                           opacity: '0.5',
                           padding: '1px',
-                          border: '1px solid red',
+                          border: '1px solid #DB5752',
                           borderRadius: '5px',
                           backgroundColor: 'white',
-                          color: 'red',
+                          color: '#DB5752',
                           fontSize: '1.8rem',
                           margin: '0 5px',
                           cursor: 'pointer',
@@ -100,7 +116,7 @@ const News = (
                           opacity: '0.5',
                           padding: '1px',
                           borderRadius: '5px',
-                          backgroundColor: 'red',
+                          backgroundColor: '#DB5752',
                           color: 'white',
                           fontSize: '1.8rem',
                           margin: '0 5px',
