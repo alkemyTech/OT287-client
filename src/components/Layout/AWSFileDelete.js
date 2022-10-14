0ac320bd27e19cc05e551d2312b62fe7e6fcf207
Guiniote <guiniote@gmail.com>
2022-10-14 15:46:55 -0300

    Implemented timestamp in file name and folder system

--- src/components/Layout/AWSFileDelete.js
+++ src/components/Layout/AWSFileDelete.js
@@ -1,19 +1,21 @@
 import ReactS3Client from 'react-aws-s3-typescript'
 
-const AWSFileDelete = async (fileName) => {
+const AWSFileDelete = async (fileName, folder) => {
   const config = {
     bucketName: process.env.REACT_APP_AWS_BUCKET_NAME,
-    dirName: process.env.REACT_APP_AWS_BUCKET_DIRNAME,
+    dirName: folder,
     region: process.env.REACT_APP_AWS_BUCKET_REGION,
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY,
     s3Url: process.env.REACT_APP_AWS_S3URL,
   }
 
+  const filePath = `${folder}/${fileName}`
+
   const s3 = new ReactS3Client(config)
 
   try {
-    await s3.deleteFile(fileName)
+    await s3.deleteFile(filePath)
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(err)
--- src/components/Layout/AWSFileUpload.js
+++ src/components/Layout/AWSFileUpload.js
@@ -1,19 +1,22 @@
 import S3 from 'react-aws-s3'
 
-const AWSFileUpload = async (file) => {
+const AWSFileUpload = async (file, folder) => {
   const config = {
     bucketName: process.env.REACT_APP_AWS_BUCKET_NAME,
-    dirName: process.env.REACT_APP_AWS_BUCKET_DIRNAME,
+    dirName: folder,
     region: process.env.REACT_APP_AWS_BUCKET_REGION,
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY,
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_KEY,
     s3Url: process.env.REACT_APP_AWS_S3URL,
   }
 
+  const date = new Date()
+  const fileNewName = `${date.getFullYear()}${date.getMonth() + 1}${date.getDate()}${date.getHours()}${date.getMinutes()}${date.getSeconds()}_${file.name}`
+
   const ReactS3Client = new S3(config)
 
   const awsPost = await ReactS3Client
-    .uploadFile(file, file.name)
+    .uploadFile(file, fileNewName)
     .then((data) => data.location)
     .catch((err) => {
       // eslint-disable-next-line no-console
