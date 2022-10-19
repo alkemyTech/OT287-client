2f5bea0fe58326e054de9f5095f576429b171e35
Corxario <federicocapucci@hotmail.com>
2022-10-19 19:30:29 -0300

    Normalizing table and images

--- src/components/BackOffice/Activities/Activities.jsx
+++ src/components/BackOffice/Activities/Activities.jsx
@@ -9,10 +9,13 @@ import EditIcon from '@mui/icons-material/Edit';
 import { Link } from 'react-router-dom';
 import DeleteModal from '../../Layout/DeleteModal'
 
+function isImage(url) {
+  return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
+}
+
 const Activities = (
   props,
 ) => {
-
   const {
     activities, handleModal, setHandleModal, setElementToDelete, elementToDelete,
     deleteElement, deletedSuccess, errorStatus, errorStatusActivities, setDeletedSuccess,
@@ -20,7 +23,7 @@ const Activities = (
 
   return (
     <>
-  <Box sx={{ padding: { xs: '10px'} }}>
+      <Box sx={{ padding: { xs: '10px' } }}>
         <Typography
           component="h1"
           variant="h5"
@@ -29,17 +32,16 @@ const Activities = (
           Lista de Actividades
         </Typography>
         <TableContainer
-
           component={Paper}
         >
           <Table>
             <TableHead>
               <TableRow sx={{ backgroundColor: 'rgb(240,240,240)' }}>
-                <TableCell sx={{lineHeight:'1'}}><b>Nombre</b></TableCell>
-                <TableCell sx={{lineHeight:'1'}}><b>URL Imagen</b></TableCell>
-                <TableCell sx={{lineHeight:'1'}}><b>Contenido</b></TableCell>
-                <TableCell sx={{lineHeight:'1'}}><b>Fecha creacion</b></TableCell>
-                <TableCell sx={{lineHeight:'1'}} align="center"><b>Acciones</b></TableCell>
+                <TableCell><b>Nombre</b></TableCell>
+                <TableCell align="center"><b>Imagen</b></TableCell>
+                <TableCell><b>Contenido</b></TableCell>
+                <TableCell><b>Fecha creacion</b></TableCell>
+                <TableCell align="center"><b>Acciones</b></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
@@ -47,64 +49,84 @@ const Activities = (
               {activities && activities.map((elem) => (
 
                 <TableRow key={elem.id}>
-                  <TableCell 
-                  sx={{ width: '25%', overflow: 'hidden', textOverflow: 'ellipsis' }}
-                  >{elem.name}</TableCell>
                   <TableCell
-                  sx={{ width: '25%', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                    sx={{ width: '15%', overflow: 'hidden', textOverflow: 'ellipsis' }}
                   >
-                    {elem.image}
+                    {elem.name}
+
                   </TableCell>
                   <TableCell
-                    sx={{ width: '25%', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                    align="center"
+                    sx={{
+                      margin: '1px', padding: '0', width: '10%',
+                    }}
+                  >
+                    <Box
+                      component="img"
+                      sx={{
+                        margin: '3px auto',
+                        height: '60px',
+                        width: '60px',
+                        objectFit: 'cover',
+                        borderRadius: '50%',
+                      }}
+                      src={isImage(elem.image) || elem.image.length > 10 ? elem.image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKH2cSl_mcI1heNKNhcs7Qpg0OVCh8AsiD5A&usqp=CAU'}
+                      alt="news image"
+                    />
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      width: '45%', textOverflow: 'ellipsis',
+                    }}
                     dangerouslySetInnerHTML={{ __html: elem.content.length < 80 ? elem.content : `${elem.content.substring(0, 80)}...` }}
                   />
                   <TableCell
-                  sx={{ width: '25%', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                    sx={{ width: '15%', overflow: 'hidden', textOverflow: 'ellipsis' }}
                   >
                     {elem.createdAt}
 
                   </TableCell>
-                  <TableCell sx={{ padding: '0', width: '25%' }} align="center">
+                  <TableCell sx={{ padding: '0', width: '10 %' }} align="center">
                     <>
-                    <Link to={`${elem.id}/edit`}>
-                      <EditIcon sx={{
-                        opacity: '0.5',
-                        padding: '1px',
-                        border: '1px solid red',
-                        borderRadius: '5px',
-                        backgroundColor: 'white',
-                        color: 'red',
-                        fontSize: '1.8rem',
-                        margin: '0 5px',
-                        cursor: 'pointer',
-                        '&:hover': { opacity: '1' },
-                      }}
-                      />
+                      <Link to={`${elem.id}/edit`}>
+                        <EditIcon sx={{
+                          opacity: '0.5',
+                          padding: '1px',
+                          border: '1px solid red',
+                          borderRadius: '5px',
+                          backgroundColor: 'white',
+                          color: 'red',
+                          fontSize: '1.8rem',
+                          margin: '0 5px',
+                          cursor: 'pointer',
+                          '&:hover': { opacity: '1' },
+                        }}
+                        />
                       </Link>
 
-                        <HighlightOffIcon sx={{
-                         opacity: '0.5',
-                         padding: '1px',
-                         borderRadius: '5px',
-                         backgroundColor: 'red',
-                         color: 'white',
-                         fontSize: '1.8rem',
-                         margin: '0 5px',
-                         cursor: 'pointer',
-                         '&:hover': { opacity: '1' },
+                      <HighlightOffIcon
+                        sx={{
+                          opacity: '0.5',
+                          padding: '1px',
+                          borderRadius: '5px',
+                          backgroundColor: 'red',
+                          color: 'white',
+                          fontSize: '1.8rem',
+                          margin: '0 5px',
+                          cursor: 'pointer',
+                          '&:hover': { opacity: '1' },
                         }}
                         onClick={() => {
                           setHandleModal(true)
                           setElementToDelete(elem)
                         }}
-                        />
+                      />
 
                     </>
                   </TableCell>
                 </TableRow>
               )) }
-               {errorStatusActivities ? 'Error al traer Novedades' : null}
+              {errorStatusActivities ? 'Error al traer Novedades' : null}
             </TableBody>
           </Table>
         </TableContainer>
@@ -119,14 +141,25 @@ const Activities = (
         setElementToDelete={setElementToDelete}
         setDeletedSuccess={setDeletedSuccess}
       />
-      </>
- )
+    </>
+  )
 }
 
 Activities.propTypes = {
   activities: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string,
+    image: PropTypes.string,
+    createdAt: PropTypes.string,
   })).isRequired,
+  handleModal: PropTypes.bool.isRequired,
+  setHandleModal: PropTypes.func.isRequired,
+  setElementToDelete: PropTypes.func.isRequired,
+  elementToDelete: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  deleteElement: PropTypes.func.isRequired,
+  deletedSuccess: PropTypes.bool.isRequired,
+  errorStatus: PropTypes.string.isRequired,
+  errorStatusActivities: PropTypes.string.isRequired,
+  setDeletedSuccess: PropTypes.func.isRequired,
 }
 export default Activities
