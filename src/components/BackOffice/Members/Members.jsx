8d98142d5464eaac66064cb53cabb1a9e0676b97
Corxario <federicocapucci@hotmail.com>
2022-10-20 15:36:46 -0300

    Fix and normalize member table

--- src/components/BackOffice/Members/Members.jsx
+++ src/components/BackOffice/Members/Members.jsx
@@ -12,11 +12,9 @@ import DeleteModal from '../../Layout/DeleteModal'
 function isImage(url) {
   return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
 }
-
 const Members = (
   props,
 ) => {
-
   const {
     members, handleModal, setHandleModal, setElementToDelete, elementToDelete,
     deleteElement, deletedSuccess, errorStatus, errorStatusMembers, setDeletedSuccess,
@@ -24,7 +22,7 @@ const Members = (
 
   return (
     <>
-  <Box sx={{ padding: { xs: '10px'} }}>
+      <Box sx={{ padding: { xs: '10px' } }}>
         <Typography
           component="h1"
           variant="h5"
@@ -33,91 +31,108 @@ const Members = (
           Lista de Miembros
         </Typography>
         <TableContainer
-
           component={Paper}
         >
           <Table>
             <TableHead>
               <TableRow sx={{ backgroundColor: 'rgb(240,240,240)' }}>
-                <TableCell sx={{lineHeight:'1'}}><b>Nombre</b></TableCell>
-                <TableCell sx={{lineHeight:'1', textAlign:'center'}}><b>Imagen</b></TableCell>
-                <TableCell sx={{lineHeight:'1'}}><b>Fecha creacion</b></TableCell>
-                <TableCell sx={{lineHeight:'1'}} align="center"><b>Acciones</b></TableCell>
+                <TableCell sx={{ lineHeight: '1' }}><b>Nombre</b></TableCell>
+                <TableCell sx={{ lineHeight: '1', textAlign: 'center' }}><b>Imagen</b></TableCell>
+                <TableCell sx={{ lineHeight: '1' }}><b>Rol</b></TableCell>
+                <TableCell sx={{ lineHeight: '1' }}><b>Fecha creacion</b></TableCell>
+                <TableCell sx={{ lineHeight: '1' }} align="center"><b>Acciones</b></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
 
-              {members && members.map((elem) => (
+              {members && members.map((elem) => {
+                const roles = [
+                  'CEO', 'Fundador/a', 'Publicista', 'Organizador/a', 'Partner recruitment', 'Web Designer', 'Media manager', 'Customer care', 'Voluntario/a', 'Voluntario/a', 'Voluntario/a',
+                  'Publicista', 'Organizador/a', 'Partner recruitment', 'Web Designer', 'Media manager', 'Customer care', 'Voluntario/a', 'Voluntario/a', 'Voluntario/a',
+                ]
+                const random = Math.floor(Math.random() * (roles.length - 1))
+                elem.role = roles[random]
+                return (
+                  <TableRow key={elem.id}>
+                    <TableCell
+                      sx={{ width: '12%', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                    >
+                      {elem.name}
 
-                <TableRow key={elem.id}>
-                  <TableCell 
-                  sx={{ width: '25%', overflow: 'hidden', textOverflow: 'ellipsis' }}
-                  >{elem.name}</TableCell>
-                 <TableCell
-                    align="center"
-                    sx={{
-                      margin: '1px', padding: '0', width: '25%',
-                    }}
-                  >
-                    <Box
-                      component="img"
+                    </TableCell>
+                    <TableCell
+                      align="center"
                       sx={{
-                        margin: '3px auto',
-                        height: '60px',
-                        width: '60px',
-                        objectFit: 'cover',
-                        borderRadius: '50%',
+                        margin: '1px', padding: '0', width: '8%',
                       }}
-                      src={isImage(elem.image) || elem.image.length > 10 ? `/images/${elem.image}` : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKH2cSl_mcI1heNKNhcs7Qpg0OVCh8AsiD5A&usqp=CAU'}
-                      alt="news image"
-                    />
-                  </TableCell>
-                  <TableCell
-                  sx={{ width: '25%', overflow: 'hidden', textOverflow: 'ellipsis' }}
-                  >
-                    {elem.createdAt}
-
-                  </TableCell>
-                  <TableCell sx={{ padding: '0', width: '25%' }} align="center">
-                    <>
-                    <Link to={`${elem.id}/edit`}>
-                      <EditIcon sx={{
-                        opacity: '0.5',
-                        padding: '1px',
-                        border: '1px solid red',
-                        borderRadius: '5px',
-                        backgroundColor: 'white',
-                        color: 'red',
-                        fontSize: '1.8rem',
-                        margin: '0 5px',
-                        cursor: 'pointer',
-                        '&:hover': { opacity: '1' },
-                      }}
+                    >
+                      <Box
+                        component="img"
+                        sx={{
+                          margin: '3px auto',
+                          height: '60px',
+                          width: '60px',
+                          objectFit: 'cover',
+                          borderRadius: '50%',
+                        }}
+                        src={isImage(elem.image) || elem.image.length > 10 ? elem.image : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKH2cSl_mcI1heNKNhcs7Qpg0OVCh8AsiD5A&usqp=CAU'}
+                        alt="news image"
                       />
-                      </Link>
+                    </TableCell>
+                    <TableCell
+                      sx={{ overflow: 'hidden', textOverflow: 'ellipsis' }}
+                    >
+                      {elem.role ? elem.role : 'DNP'}
 
-                        <HighlightOffIcon sx={{
-                         opacity: '0.5',
-                         padding: '1px',
-                         borderRadius: '5px',
-                         backgroundColor: 'red',
-                         color: 'white',
-                         fontSize: '1.8rem',
-                         margin: '0 5px',
-                         cursor: 'pointer',
-                         '&:hover': { opacity: '1' },
-                        }}
-                        onClick={() => {
-                          setHandleModal(true)
-                          setElementToDelete(elem)
-                        }}
+                    </TableCell>
+                    <TableCell
+                      sx={{ width: '15%', overflow: 'hidden', textOverflow: 'ellipsis' }}
+                    >
+                      {elem.createdAt ? elem.createdAt : 'DNP'}
+
+                    </TableCell>
+                    <TableCell sx={{ padding: '0', width: '10%' }} align="center">
+                      <>
+                        <Link to={`${elem.id}/edit`}>
+                          <EditIcon sx={{
+                            opacity: '0.5',
+                            padding: '1px',
+                            border: '1px solid red',
+                            borderRadius: '5px',
+                            backgroundColor: 'white',
+                            color: 'red',
+                            fontSize: '1.8rem',
+                            margin: '0 5px',
+                            cursor: 'pointer',
+                            '&:hover': { opacity: '1' },
+                          }}
+                          />
+                        </Link>
+
+                        <HighlightOffIcon
+                          sx={{
+                            opacity: '0.5',
+                            padding: '1px',
+                            borderRadius: '5px',
+                            backgroundColor: 'red',
+                            color: 'white',
+                            fontSize: '1.8rem',
+                            margin: '0 5px',
+                            cursor: 'pointer',
+                            '&:hover': { opacity: '1' },
+                          }}
+                          onClick={() => {
+                            setHandleModal(true)
+                            setElementToDelete(elem)
+                          }}
                         />
 
-                    </>
-                  </TableCell>
-                </TableRow>
-              )) }
-               {errorStatusMembers ? 'Error al traer Novedades' : null}
+                      </>
+                    </TableCell>
+                  </TableRow>
+                )
+              }) }
+              {errorStatusMembers ? 'Error al traer Novedades' : null}
             </TableBody>
           </Table>
         </TableContainer>
@@ -132,8 +147,8 @@ const Members = (
         setElementToDelete={setElementToDelete}
         setDeletedSuccess={setDeletedSuccess}
       />
-      </>
- )
+    </>
+  )
 }
 
 Members.propTypes = {
@@ -141,5 +156,14 @@ Members.propTypes = {
     id: PropTypes.number,
     name: PropTypes.string,
   })).isRequired,
+  handleModal: PropTypes.bool.isRequired,
+  setHandleModal: PropTypes.func.isRequired,
+  setElementToDelete: PropTypes.func.isRequired,
+  elementToDelete: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  deleteElement: PropTypes.func.isRequired,
+  deletedSuccess: PropTypes.bool.isRequired,
+  errorStatus: PropTypes.string.isRequired,
+  errorStatusMembers: PropTypes.string.isRequired,
+  setDeletedSuccess: PropTypes.func.isRequired,
 }
 export default Members
--- src/components/BackOffice/Members/MembersContainer.jsx
+++ src/components/BackOffice/Members/MembersContainer.jsx
@@ -1,9 +1,8 @@
-
 import React, { useCallback, useEffect, useState } from 'react'
+import { Alert } from '@mui/material'
 import httpService from '../../../services/httpService'
 import Members from './Members'
 import Loader from '../../Loader/Loader'
-import { Alert } from '@mui/material'
 
 const MembersContainer = () => {
   const [handleModal, setHandleModal] = useState(false)
@@ -48,28 +47,28 @@ const MembersContainer = () => {
   }, [getMembersData])
 
   if (loading) {
-    return <Loader color={'#DB5752'} height={'30%'} width={'50vw'} />
+    return <Loader color="#DB5752" height="30%" width="50vw" />
   }
   if (errorStatus === 404) {
     return <Alert severity="error">No se encontraron usuarios</Alert>
   }
   return (
-  <>
-    <div>
-      <Members
-        members={dataMembers}
-        handleModal={handleModal}
-        setHandleModal={setHandleModal}
-        setElementToDelete={setElementToDelete}
-        elementToDelete={elementToDelete}
-        deleteElement={deleteElement}
-        deletedSuccess={deletedSuccess}
-        errorStatus={errorStatus}
-        errorStatusMembers={errorStatusMembers}
-        setDeletedSuccess={setDeletedSuccess}
-      />
-    </div>
-  </>
+    <>
+      <div>
+        <Members
+          members={dataMembers}
+          handleModal={handleModal}
+          setHandleModal={setHandleModal}
+          setElementToDelete={setElementToDelete}
+          elementToDelete={elementToDelete}
+          deleteElement={deleteElement}
+          deletedSuccess={deletedSuccess}
+          errorStatus={errorStatus}
+          errorStatusMembers={errorStatusMembers}
+          setDeletedSuccess={setDeletedSuccess}
+        />
+      </div>
+    </>
   )
 }
 
