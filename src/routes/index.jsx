3b853c4bf5fb569a1187e27e507023333d1a20ad
Guiniote <guiniote@gmail.com>
2022-10-22 18:10:47 -0300

    Frontend Console Fixes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -58,7 +58,8 @@ const Router = () => {
               <Route path="/logout" element={<Logout />} />
               <Route path="/login" element={<LoginFormContainer />} />
               <Route path="/registrate" element={<RegisterFormContainer />} />
-              <Route path="/mi-perfil/editar" element={<PrivateRoute><EditUserProfileContainer /></PrivateRoute>} />
+              <Route path="/mi-perfil" element={<MyProfileContainer />} />
+              <Route path="/mi-perfil/editar" element={<EditUserProfileContainer />} />
               <Route path="/mi-perfil" element={<PrivateRoute><MyProfileContainer /></PrivateRoute>} />
               <Route path="/novedades" element={<NewsContainer />} />
               <Route path="/novedades/:id" element={<NewsByIdContainer />} />
